8c3f838f595f0f9c4ef66873e6e5955afc44fa54
davidhenshaw <davidhenshaw2014@gmail.com>
2021-06-02 10:03:30 -0500

    Restructured database schema and re-seeded

--- backend/db/migrate/20210601193143_create_choice.rb
+++ backend/db/migrate/20210601193143_create_choice.rb
@@ -2,7 +2,8 @@ class CreateChoice < ActiveRecord::Migration[5.2]
   def change
     create_table :choices do |t|
       t.string :text
-      t.integer :scene_id
+      t.integer :to_scene_id
+      t.integer :from_scene_id
     end
   end
 end
