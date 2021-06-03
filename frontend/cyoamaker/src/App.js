5408cdeb0bb0c8dd9e5f38e511895bda5878f100
davidhenshaw <davidhenshaw2014@gmail.com>
2021-06-03 11:35:05 -0500

    Install and set up react-router-dom

--- frontend/cyoamaker/src/App.js
+++ frontend/cyoamaker/src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home'
+import StoryEditor from './components/StoryEditor';
 
 class App extends React.Component{
 
 
   render(){
     return(
-      <div>
-        <Header/>
-        <Home/>
-      </div>
+        <Router>
+          <div>
+          <Header />
+          <Switch>
+            <Route path="/story-editor">
+              <StoryEditor />
+            </Route>
+            <Route path="/">
+              <Home />               
+            </Route>
+          </Switch>
+          </div>
+        </Router>
     )
   }
 
---
+++ frontend/cyoamaker/src/components/StoryEditor.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class StoryEditor extends React.Component{
+
+    render(){
+        return <div>Story Editor</div>
+    }
+ 
+}
+
+export default StoryEditor;
