37d6223b09681982b0fd4cd29ff1cadf19d16bbe
davidhenshaw <davidhenshaw2014@gmail.com>
2021-06-04 11:43:29 -0500

    Made a better title

--- frontend/cyoamaker/src/components/Home.jsx
+++ frontend/cyoamaker/src/components/Home.jsx
@@ -7,7 +7,7 @@ class Home extends React.Component{
     render(){
         return(
             <div className="textbox">
-                <p className="intro"><p className="h2Intro">Instructions:</p>Welcome to David's Adventure! This is a build your own adventure game. First you will be asked to make a story name with a description.Once you have created a story, please head to My Story where your will be able to start creating.</p>
+                <p className="intro"><p className="h2Intro">Instructions:</p>Welcome to A & D Adventure Builder! This is a build your own adventure game. First you will be asked to make a story name with a description.Once you have created a story, please head to My Story where your will be able to start creating.</p>
                 <Body onStoryPost={this.props.onStoryPost}/>
                 <StoryContainer onSelectStory={this.props.onSelectStory} user={this.props.user} stories={this.props.stories}/>
             </div>
